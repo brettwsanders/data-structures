efc81905a7433d29f3ebfc221cc39cc837c904f9
brettwsanders <bsand08@gmail.com>
2015-08-20 11:31:09 -0700

    Solved the Linked List problem.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,33 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = Node(value);
+    list[value] = node;
+    if (list.head === null && list.tail === null) {
+      list.head = list[value];
+      list.tail = list[value];
+    } else {
+      list.tail.next = list[value];
+      list.tail = list[value];
+    }
+
   };
 
   list.removeHead = function(){
+
+    var next = list.head.next;
+    var removed = list.head;
+    delete list[list.head.value];
+    list.head = next;
+    return removed.value;
   };
 
   list.contains = function(target){
+    for (var key in list) {
+      if(key === target.toString())
+        return true;
+    }
+    return false;
   };
 
   return list;
